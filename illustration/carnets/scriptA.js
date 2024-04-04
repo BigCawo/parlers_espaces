2ab535e7bf3a7de19c3e83751d6f61c7be795377
Eliot Bernaud <43908605+BigCawo@users.noreply.github.com>
2024-04-04 13:35:22 +0200

    Add files via upload

--- illustration/carnets/scriptA.js
+++ illustration/carnets/scriptA.js
@@ -32,6 +32,8 @@ SliderDetector.oninput = function() {
 
 }
 
+
+
 document.body.onload=function(){
 
 
@@ -50,6 +52,7 @@ document.body.onload=function(){
         div = document.createElement("div");
         div.className="carouselPhotoA"
         div.style.backgroundImage="url('../../assets/carnets/2023_10_A4/"+(i-1)+".jpg')";
+        div.style.scrollSnapAlign = "start"
         carouselContainer.appendChild(div);
     }
     affichermasquer();
@@ -64,6 +67,8 @@ sh = window.innerHeight
 sw = window.innerWidth
 
 t = (mainW - ((wA/hA)*0.8*sh))/2
+
+// document.getElementById("input").innerHTML= document.getElementById("carouselA").scrollLeft
 
 
 
